4e28006a54f468664f4b2606daf37b1734b62578
AGTECH101 <ejiofor707@gmail.com>
2025-11-22 02:54:24 +0100

    lfc upload day2 trial4

--- project/wsgi.py
+++ project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lfc_teens_project.settings')  # CHANGED to match your project
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')  # CHANGED to match your project
 
 application = get_wsgi_application()
